2490dc86bb609fb129fa91174787c805a5974203
Lucas Montenoise <montenoise.lucas@gmail.com>
2020-03-09 09:48:39 +0100

    Added a last comment

--- src/pages/Messages/Messages.js
+++ src/pages/Messages/Messages.js
@@ -8,6 +8,13 @@ const Messages = ({ username }) => {
 	const [messages, setMessages] = useState([]);
 	const authorizedMessages = messages.filter(msg => !msg.isPrivate || msg.author === username);
 
+	/*
+	 * Dans le cadre de cet exercice, le système de polling mis en place n'a bien entendu pas de sens,
+	 * car personne ne peut réellement m'envoyer de messages.
+	 * Je l'ai fait pour simplement illustrer le fait que je sais le mettre en place.
+	 * Dans un projet réel, si j'avais eu la main sur l'API j'aurais probablement mis en place
+	 * un système de long polling, ou même de sockets via par exemple socket.io.
+     */
 	useEffect(() => {
 		fetchMessages(setMessages);
 		const intervalId = setInterval(() => {
